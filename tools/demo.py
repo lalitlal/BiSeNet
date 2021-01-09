59724d346c962b5901ffee66894d45802788af4e
lalitlal <lalitlal23@hotmail.com>
2021-01-09 14:35:08 -0500

    Update demo.py

--- tools/demo.py
+++ tools/demo.py
@@ -11,6 +11,7 @@ import cv2
 import lib.transform_cv2 as T
 from lib.models import model_factory
 from configs import cfg_factory
+from labels import *
 
 torch.set_grad_enabled(False)
 np.random.seed(123)
@@ -26,9 +27,31 @@ cfg = cfg_factory[args.model]
 
 
 palette = np.random.randint(0, 256, (256, 3), dtype=np.uint8)
+cityspallete = [
+    128, 64, 128,
+    244, 35, 232,
+    70, 70, 70,
+    102, 102, 156,
+    190, 153, 153,
+    153, 153, 153,
+    250, 170, 30,
+    220, 220, 0,
+    107, 142, 35,
+    152, 251, 152,
+    0, 130, 180,
+    220, 20, 60,
+    255, 0, 0,
+    0, 0, 142,
+    0, 0, 70,
+    0, 60, 100,
+    0, 80, 100,
+    0, 0, 230,
+    119, 11, 32,
+]
 
 # define model
 net = model_factory[cfg.model_type](19)
+
 net.load_state_dict(torch.load(args.weight_path, map_location='cpu'))
 net.eval()
 net.cuda()
@@ -43,5 +66,16 @@ im = to_tensor(dict(im=im, lb=None))['im'].unsqueeze(0).cuda()
 
 # inference
 out = net(im)[0].argmax(dim=1).squeeze().detach().cpu().numpy()
-pred = palette[out]
-cv2.imwrite('./res.jpg', pred)
+h,w = np.shape(out)
+finalout = np.empty((h,w))
+for i in range(h):
+    for j in range(w):
+#         print(out[i,j])
+        finalout[i,j] = trainId2label[out[i,j]].id
+
+pred = Image.fromarray(out.astype('uint8'))
+pred.putpalette(cityspallete)
+# pred = cityspallete[pred]
+# cv2.imwrite('./res.jpg', pred)
+pred.save('./res.png')
+
