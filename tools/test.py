4f549130611daf58b43cb8b4d80cfcb5949f972b
lalitlal <lalitlal23@hotmail.com>
2021-01-09 14:33:11 -0500

    Update test.py

--- tools/test.py
+++ tools/test.py
@@ -14,6 +14,8 @@ from os.path import basename
 import lib.transform_cv2 as T
 from lib.models import model_factory
 from configs import cfg_factory
+import time
+from labels import *
 
 torch.set_grad_enabled(False)
 np.random.seed(123)
@@ -29,6 +31,27 @@ cfg = cfg_factory[args.model]
 
 
 palette = np.random.randint(0, 256, (256, 3), dtype=np.uint8)
+cityspallete = [
+    128, 64, 128,
+    244, 35, 232,
+    70, 70, 70,
+    102, 102, 156,
+    190, 153, 153,
+    153, 153, 153,
+    250, 170, 30,
+    220, 220, 0,
+    107, 142, 35,
+    152, 251, 152,
+    0, 130, 180,
+    220, 20, 60,
+    255, 0, 0,
+    0, 0, 142,
+    0, 0, 70,
+    0, 60, 100,
+    0, 80, 100,
+    0, 0, 230,
+    119, 11, 32,
+]
 
 # define model
 net = model_factory[cfg.model_type](19)
@@ -43,22 +66,44 @@ to_tensor = T.ToTensor(
 )
 
 # directory structure:
-rootdir = '../datasets/leftImg8bit/test'
-respath = '.testres/'
+# rootdir = '../datasets/leftImg8bit/test'
+# respath = '.testres/'
+d = os.path.dirname(os.getcwd())
+rootdir = d + '/' + 'BiSeNet/datasets/cityscapes/leftImg8bit/test'
+respath = d + '/BiSeNet/testsubmission/'
 # go through each image and output it into results:
+
+start_time = time.time()
+num_pics = 0
+end = 0
+total_time = 0
+num_images = 0
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
         im = cv2.imread(os.path.join(subdir, file))[:, :, ::-1]
         im = to_tensor(dict(im=im, lb=None))['im'].unsqueeze(0).cuda()
-
+        start_time = time.time()
         # inference
-        out = net(im)[0].argmax(dim=1).squeeze().detach().cpu().numpy()
-        pred = palette[out]
-
-        dirname = respath + basename(subdir)
-        print(dirname)
+        out = net(im)[0]
+        total_time += time.time() - start_time
+        out = out.argmax(dim=1).squeeze().detach().cpu().numpy()
+#         pred = Image.fromarray(out.astype('uint8'))
+#         pred.putpalette(cityspallete)
+        
+        # for the submission!
+        h,w = np.shape(out)
+        finalout = np.empty((h,w))
+        for i in range(h):
+            for j in range(w):
+                finalout[i,j] = trainId2label[out[i,j]].id
+        subFile = Image.fromarray(finalout.astype('uint8'))
+        dirname = respath + '/' + basename(subdir)
         if not os.path.exists(dirname):
-            os.mkdir(dirname)
+            os.makedirs(dirname)
         outpath = dirname + '/' + basename(file)
-        cv2.imwrite(outpath, pred)
+        subFile.save(outpath)
+        num_images += 1
+
+print(num_images, total_time, num_images/total_time)
+
 
